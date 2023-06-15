f912d28b2eb6120c0247168f5e7878ef1c2badcd
MarioAndWeegee3 <marioandweegee3@gmail.com>
2023-06-15 17:13:41 -0400

    # Version 3.0.0
    * Ported to 1.20.1
    * All elements are represented by a button. For types other than lists and objects, this button does nothing and is
    purely decorative.

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # NBT Viewer
 
-Release 2.0.0 and later are fully stable and OK to use in mod packs.
-Link to the CurseForge, Modrinth, or GitHub page if you include it in
-a pack.
-
 ## Usage
 
 There is a configurable key bind - Right ALT by default - which opens the 
@@ -13,3 +9,11 @@ Inside the GUI, there will be key-value pairs for each element of the data.
 For Lists, the index in the list is the key.
 
 Object keys are sorted by name.
+
+## Changelog
+
+### Version 3.0.0
+
+* Ported to 1.20.1
+* All elements are represented by a button. For types other than lists and objects, this button does nothing and is
+purely decorative.
--- src/main/kotlin/marioandweegee3/nbtviewer/gui/NBTViewerScreen.kt
+++ src/main/kotlin/marioandweegee3/nbtviewer/gui/NBTViewerScreen.kt
@@ -3,16 +3,15 @@ package marioandweegee3.nbtviewer.gui
 import io.github.cottonmc.cotton.gui.client.CottonClientScreen
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
-import net.minecraft.text.LiteralText
-import net.minecraft.text.TranslatableText
+import net.minecraft.text.Text
 
 @Environment(EnvType.CLIENT)
 class NBTViewerScreen(
     description: NBTViewerGui,
 ): CottonClientScreen(
-    if (description.name === null)
-        TranslatableText("text.nbtviewer.title")
-    else
-        LiteralText(description.name),
+    when (description.name) {
+        null -> Text.translatable("text.nbtviewer.title")
+        else -> Text.literal(description.name)
+    },
     description
 )
